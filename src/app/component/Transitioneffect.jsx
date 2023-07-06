eee4ac8e7c7c96ac25045c7f390d2256d990bf96
rafandark <90086345+rafandark@users.noreply.github.com>
2023-07-06 13:17:19 +0600

    color changed

--- src/app/component/Transitioneffect.jsx
+++ src/app/component/Transitioneffect.jsx
@@ -9,14 +9,14 @@ const Transitioneffect = () => {
     <>
     
     <motion.div className="
-     fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2d99ac]"
+     fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#fffb0d]"
      initial ={{x:'100%' ,width :'100%'}}
      animate ={{x:'0%',width :'0%'}}
      transition ={{duration:1.2, ease:"easeInOut"}}
      exit={{x:['0%','100%'], width:['0%' ,'100%']}}
      />
      <motion.div className="
-     fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#9bff29]"
+     fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#50ff29]"
      initial ={{x:'100%' ,width :'100%'}}
      animate ={{x:'0%',width :'0%'}}
      transition ={{delay:0.2, duration:1.2 , ease:"easeInOut"}}
